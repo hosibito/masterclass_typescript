e7745d6600f6146fffe4f8638d263b81b738c565
hosibito <hosibito@naver.com>
2022-03-15 18:30:54 +0900

    4.1 Styles(resetcss)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,92 @@
+import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor}
+}
+a {
+  text-decoration:none;
+}
+`;
+
 function App() {
-  return <Router />; 
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
 
+
+/**
+  resetcss 를 한다. 
+    테마를 적용했고  ThemeProvider 내에 App 이 있으므로 
+      background-color:${(props) => props.theme.bgColor};
+      color:${(props) => props.theme.textColor}
+      이게 가능하다!!
+
+
+    현제 처리 방식으로 처리하면 JS console 에서 
+      createGlobalStyle.js:46 Please do not use @import CSS syntax in createGlobalStyle at this time, as the CSSOM APIs we use in production do not handle it well. Instead, we recommend using a library such as react-helmet to inject a typical <link> meta tag to the stylesheet, or simply embedding it manually in your index.html <head> section for a simpler app.
+      
+      createGlobalStyle.js:46 현재 createGlobalStyle에서 @import CSS 구문을 사용하지 마세요. 프로덕션에서 사용하는 CSSOM API가 제대로 처리하지 못하기 때문입니다. 대신 react-helmet과 같은 라이브러리를 사용하여 스타일시트에 일반적인 <link> 메타 태그를 삽입하거나 더 간단한 앱을 위해 index.html <head> 섹션에 수동으로 포함하는 것이 좋습니다.
+    이런 불평을 한다. 
+  
+ */
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -9,15 +9,3 @@ function Coin() {
 }
 export default Coin;
 
-/**
- react-router-dom v6 이상이신 분들은
-    const { coinId } = useParams(); 이렇게만 해주셔도 됩니다.
-    useParams쓰는 순간 타입이 string or undefined로 됩니다.
-
-    interface RouteParams {
-        coinId: string;
-        }
-    도 필요없다.. 주소창에 파라메터로 넘어오는건 srting 일수 밖에 없다..
-
- react-query가 자체적으로 type정의를 제공하여 다운로드가 필요 없어짐
- */
