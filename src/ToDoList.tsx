395697ab7aaed57b134273fcbf7497e9743b89ae
hosibito <hosibito@naver.com>
2022-03-21 20:22:12 +0900

    5.8 Form Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 // function ToDoList() {
@@ -29,34 +28,94 @@ import { useForm } from "react-hook-form";
 //     </div>
 //   );
 // }
+
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password1: string;
+}
+
 function ToDoList() {
-    const { register, watch , handleSubmit, formState } = useForm();
-    //console.log(watch());  //폼 내부 데이터가 변화하는걸 감지
-    const onValid = (data: any) => {  // 폼에서 submit 를 누르면 발리데이션 검사를 해서 문제없으면 값을 받아옴
+    const { 
+        register , 
+        handleSubmit, 
+        formState : {errors},
+    } = useForm<IForm>(
+        {
+            defaultValues:{
+                email: "@naver.com",
+            }
+        }
+    );
+
+    const onValid = (data: any) => {  
         console.log(data);
     };
-    console.log(formState.errors);
+
     return (
       <div>
         <form
             style={{ display: "flex", flexDirection: "column" }}    
             onSubmit={handleSubmit(onValid)}
         >        
-          <input {...register( "email",  { required: true } )} placeholder="Email" />
-          <input {...register( "firstName",  { required: true } )} placeholder="First Name" />
-          <input {...register( "lastName",  { required: true } )} placeholder="Last Name" />
-          <input {...register( "username",  { required: true , minLength: 10} )} placeholder="Username" />
-          <input {...register( "password",  { required: true , minLength: 10 } )} placeholder="Password" />
-          <input 
-            {...register( "password1",  { 
-                required: "Password is required" , 
-                minLength:  {
-                    value: 5,
-                    message: "Your password is too short.",
-                  },
+            <input 
+                {...register(
+                        "email", {
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                                message: "Only naver.com emails allowed",
+                            },
+                        }
+                    )
+                }                    
+                placeholder="Email"
+            />
+            <span>{errors?.email?.message}</span>
 
-            } )} placeholder="Password1" />
-          <button>Add</button>
+            <input
+                {...register("firstName", { required: "write here" })}
+                placeholder="First Name"
+            />
+            <span>{errors?.firstName?.message}</span>
+
+            <input  
+                {...register("lastName", { required: "write here" })}
+                placeholder="Last Name"
+            />
+            <span>{errors?.lastName?.message}</span>
+
+            <input 
+                {...register("username", { required: "write here", minLength: 10 })}
+                placeholder="Username"
+            />
+            <span>{errors?.username?.message}</span>
+
+            <input 
+                {...register("password", { required: "write here", minLength: 5 })}
+                placeholder="Password"
+            />
+            <span>{errors?.password?.message}</span>
+
+            <input 
+                {
+                    ...register( 
+                        "password1",  
+                        { 
+                            required: "Password is required" , 
+                            minLength:  {
+                                value: 5,
+                                message: "Your password is too short.",
+                            },
+                        }
+                    )
+                } 
+                placeholder="Password1" />
+            <span>{errors?.password1?.message}</span>
+            <button>Add</button>
         </form>
       </div>
     );
