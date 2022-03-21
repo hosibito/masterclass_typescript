590c1bf0aa4f1bab09c532caf7405567698bad23
hosibito <hosibito@naver.com>
2022-03-21 18:17:07 +0900

    5.7 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -30,17 +30,32 @@ import { useForm } from "react-hook-form";
 //   );
 // }
 function ToDoList() {
-    const { register, watch } = useForm();
-    console.log(watch());
+    const { register, watch , handleSubmit, formState } = useForm();
+    //console.log(watch());  //폼 내부 데이터가 변화하는걸 감지
+    const onValid = (data: any) => {  // 폼에서 submit 를 누르면 발리데이션 검사를 해서 문제없으면 값을 받아옴
+        console.log(data);
+    };
+    console.log(formState.errors);
     return (
       <div>
-        <form>
-          <input {...register("email")} placeholder="Email" />
-          <input {...register("firstName")} placeholder="First Name" />
-          <input {...register("lastName")} placeholder="Last Name" />
-          <input {...register("username")} placeholder="Username" />
-          <input {...register("password")} placeholder="Password" />
-          <input {...register("password1")} placeholder="Password1" />
+        <form
+            style={{ display: "flex", flexDirection: "column" }}    
+            onSubmit={handleSubmit(onValid)}
+        >        
+          <input {...register( "email",  { required: true } )} placeholder="Email" />
+          <input {...register( "firstName",  { required: true } )} placeholder="First Name" />
+          <input {...register( "lastName",  { required: true } )} placeholder="Last Name" />
+          <input {...register( "username",  { required: true , minLength: 10} )} placeholder="Username" />
+          <input {...register( "password",  { required: true , minLength: 10 } )} placeholder="Password" />
+          <input 
+            {...register( "password1",  { 
+                required: "Password is required" , 
+                minLength:  {
+                    value: 5,
+                    message: "Your password is too short.",
+                  },
+
+            } )} placeholder="Password1" />
           <button>Add</button>
         </form>
       </div>
