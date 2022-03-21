40403b07c206ca55924e3a21150943b32226b966
hosibito <hosibito@naver.com>
2022-03-21 12:10:15 +0900

    5.3 Introduction to Recoil part Two

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,10 @@
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { fetchCoins } from "../api";
+import { isDarkAtom } from "../atoms";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -66,6 +68,8 @@ interface ICoinsProps {
 }
 
 function Coins() {
+    const setDarkAtom = useSetRecoilState(isDarkAtom);
+    const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
     const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
    
     return (
@@ -77,6 +81,7 @@ function Coins() {
             </HelmetProvider>
             <Header>
               <Title>코인</Title>
+              <button onClick={toggleDarkAtom}>Toggle Mode</button>
             </Header>
             {isLoading ? (
                 <Loader>Loading...</Loader>
