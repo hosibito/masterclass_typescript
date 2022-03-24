0da076eb6b06c6199e4df9167fbb091f2666aca0
hosibito <hosibito@naver.com>
2022-03-24 15:06:02 +0900

    6.11 Droppable Snapshot

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -9,8 +9,7 @@ export const toDoState = atom<IToDoState>({
   default: {
     "To Do": ["a", "b"],
     Doing: ["c", "d", "e"],
-    Done: ["f"],
-    "Do Later": ["x", "z"],
+    Done: ["f"],  
   },
 });
 
