8f99d7dc75b00a1b4794d93a27c59120a5ea3a96
hosibito <hosibito@naver.com>
2022-03-24 20:33:47 +0900

    7.0 Introduction

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,11 +1,7 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme {    
-    bgColor: string;
-    boardColor: string;
-    cardColor: string;
-  }
+  export interface DefaultTheme {}
 }
 
 /**
