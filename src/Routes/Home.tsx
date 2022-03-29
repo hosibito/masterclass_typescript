8aec20b68305831f1e3fd9acfb1fd91f349d920b
hosibito <hosibito@naver.com>
2022-03-29 21:57:04 +0900

    8.2 Header part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,4 @@
 function Home() {
-    return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
+    return <div style={{ backgroundColor: "whitesmoke", height: "200vh" , marginTop:"100px"}}>Home</div>;
   }
   export default Home;
