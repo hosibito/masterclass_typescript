9b258c1120ff39fed54703fbe6fbbe5cc96751be
hosibito <hosibito@naver.com>
2022-03-30 16:56:39 +0900

    8.7 Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
-
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background: black;
@@ -15,19 +16,20 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div<{bgPhotoUrl :string}>`
+const Banner = styled.div<{ bgPhoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
-  background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${(props) => props.bgPhotoUrl});
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
   background-size: cover;
 `;
 
 const Title = styled.h2`
   font-size: 68px;
-  margin-bottom: 20px;
+  margin-bottom: 20px; ;
 `;
 
 const Overview = styled.p`
@@ -35,24 +37,77 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
 function Home() {
-  const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
-  console.log(data, isLoading);
-  console.log(makeImagePath(data?.results[0].backdrop_path || ""))
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
   return (
     <Wrapper>
-      {isLoading ? ( 
-        <Loader>Loding...</Loader>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhotoUrl={makeImagePath(data?.results[0].backdrop_path || "")}> 
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
-          </Banner> 
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
   );
 }
 export default Home;
-
