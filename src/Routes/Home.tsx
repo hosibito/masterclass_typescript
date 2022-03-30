bae2c893b427e8399ce5d1b160d45ba341e49e2c
hosibito <hosibito@naver.com>
2022-03-30 21:44:13 +0900

    8.13 Movie Modal part Three

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -97,8 +97,32 @@ const BigMovie = styled(motion.div)<{scrolly:number}>`
   top: ${(props) => props.scrolly + 100}px;
   left: 0;
   right: 0;
-  margin: 0 auto;
-  background-color: tomato;
+  margin: 0 auto;  
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVariants = {
@@ -167,6 +191,11 @@ function Home() {
   };
 
   const onOverlayClick = () => navigate(`/`);
+  const clickedMovie = 
+      moviePathMatch?.params.movieId && 
+      data?.results.find(
+        (movie) => movie.id === Number(moviePathMatch?.params.movieId) 
+      );
   return (
     <Wrapper>
       {isLoading ? (
@@ -225,6 +254,20 @@ function Home() {
                   layoutId={moviePathMatch.params.movieId}   
                   scrolly={scrollY.get()}              
                 >
+                  {clickedMovie && (
+                    <>
+                      <BigCover
+                        style={{
+                          backgroundImage: 
+                            `linear-gradient(to top, black, transparent), 
+                            url(${makeImagePath(clickedMovie.backdrop_path,"w500"
+                          )})`,
+                        }}
+                      />
+                      <BigTitle>{clickedMovie.title}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
                 </BigMovie>
               </>
             ) : null}
